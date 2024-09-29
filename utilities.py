2cd4b7352dbfe2a6a159ad4cfb9c69e41b5c98f1
Thoalfeqar-gata <thoalfeqarkatajarullah@gmail.com>
2024-09-29 19:05:46 +0300

    Added additional activation functions, fixed some issues, and made the output layer take any activation function instead of just linear

--- utilities.py
+++ utilities.py
@@ -8,25 +8,68 @@ def sigmoid_d(n):
     a = sigmoid(n)
     return (1 - a) * a
 
+def bipolar_sigmoid(n):
+    return 2 * sigmoid(n) - 1
+
+def bipolar_sigmoid_d(n):
+    a = bipolar_sigmoid(n)
+    return 1/2 * (1 - a**2)
+
+def tanh(n):
+    return np.tanh(n)
+
+def tanh_d(n):
+    a = tanh(n)
+    return 1 - a**2 
+
+def relu(n):
+    result = np.copy(n)
+    result[result <= 0] = 0
+    return result
+
+def relu_d(n):
+    result = np.copy(n)
+    result[result > 0] = 1
+    result[result <= 0] = 0
+    return result
+
+def linear(n):
+    return n
+
+def linear_d(n):
+    return np.ones(shape = n.shape)
+
+
 #define a neural network class for the mean squared error loss function
 class NeuralNetwork:
-    def __init__(self, layers, learning_rate = 0.1, initialization = 'random', hidden_layer_activations = 'sigmoid'):
+    def __init__(self, layers, layer_activations, learning_rate = 0.1, initialization = 'random', hidden_layer_activations = 'sigmoid'):
         self.W = [] #weights
         self.b = [] #biases
         self.__a = [] #activations of each layer
         self.__n = [] #net input into each layer
         self.__s = [] #sensitivities of each layer
-        self.activations = { #a dict of all possible activation functions for the hidden layers
+        
+        self.__layer_activations = layer_activations #a list of the activation functions for each layer
+        self.__activations = { #a dict of all possible activation functions and their derivatives
             'sigmoid': sigmoid,
-            'sigmoid_d' : sigmoid_d
+            'sigmoid_d' : sigmoid_d,
+            'linear' : linear,
+            'linear_d' : linear_d,
+            'relu' : relu,
+            'relu_d' : relu_d,
+            'bipolar_sigmoid' : bipolar_sigmoid,
+            'bipolar_sigmoid_d' : bipolar_sigmoid_d,
+            'tanh' : tanh,
+            'tanh_d' : tanh_d
         }
+        
         self.num_layers = len(layers)
         self.learning_rate = learning_rate
         
-        #the activation function of the hidden layers
-        self.F = self.activations[hidden_layer_activations] 
-        #the derivative of the activation function for the hidden layers
-        self.F_d = self.activations[hidden_layer_activations + '_d'] 
+        #the activation functions of the layers
+        self.F = [self.__activations[layer_activation] for layer_activation in self.__layer_activations]
+        #the derivative of the activation function for the layers
+        self.F_d = [self.__activations[layer_activation + '_d'] for layer_activation in self.__layer_activations] 
         
         #initialize the parameters
         for i in range(self.num_layers - 1):
@@ -55,26 +98,26 @@ class NeuralNetwork:
         
         #first layer
         self.__n[0] = self.W[0] @ X + self.b[0]
-        self.__a[0] = self.F(self.__n[0])
+        self.__a[0] = self.F[0](self.__n[0])
         
         #hidden layers
         for i in range(1, self.num_layers - 1):
-            self.__n[i] = self.W[i] @ self.__a[i - 1] + self.b[0]
-            self.__a[i] = self.F(self.__n[i])
+            self.__n[i] = self.W[i] @ self.__a[i - 1] + self.b[i]
+            self.__a[i] = self.F[i](self.__n[i])
         
         #output layer
-        self.__a[-1] = self.W[-1] @ self.__a[-2] + self.b[-1]
+        self.__n[-1] = self.W[-1] @ self.__a[-2] + self.b[-1]
+        self.__a[-1] = self.F[-1](self.__n[-1])
         
         return self.__a[-1]
     
     
     def __backward_pass(self, y):
-        
-        self.__s[-1] = -2 * 1 * (y - self.__a[-1]) #final layer sensitivity
+        self.__s[-1] = -2 * self.F_d[-1](self.__n[-1]) * (y - self.__a[-1]) #final layer sensitivity
         
         #remaining layers
         for i in range(self.num_layers - 3, -1, -1):
-            self.__s[i] = self.F_d(self.__n[i]) * self.W[i + 1].T @ self.__s[i+1]
+            self.__s[i] = self.F_d[i](self.__n[i]) * self.W[i + 1].T @ self.__s[i+1]
 
         return self.__s
     
